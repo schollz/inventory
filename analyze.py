3e5868bfd0f279e11c9e48bdff9e077f4c9ff6b7
Zack <zack.scholl@gmail.com>
2015-11-05 07:45:52 -0500

    Now generates grocery list

--- analyze.py
+++ analyze.py
@@ -1,16 +1,34 @@
+from datetime import date
+import numpy
+import json
+
+today = date.today()
 foods = {}
+allDays = []
+
 with open('list.csv','r') as f:
     for line in f:
         csvs = line.split(',')
         if '/' in csvs[0]:
             monthDate = csvs[0].split()[0]
-            days = 30*int(monthDate.split('/')[0])
-            days += 1*int(monthDate.split('/')[1])
-            days += 365*int(monthDate.split('/')[2])
+            day = date(2000+int(monthDate.split('/')[2]),int(monthDate.split('/')[0]),int(monthDate.split('/')[1]))
+            days = abs(today-day).days
+            allDays.append(days)
             for food in csvs[1:]:
                 f = food.strip()
                 if f not in foods:
                     foods[f] = []
                 foods[f].append(days)
 
-print(foods)
+
+firstDay = max(allDays)
+groceries = {}
+for food in foods:
+    groceries[food] = {}
+    groceries[food]['meanWaitTime'] = numpy.mean(numpy.diff(sorted([0] + foods[food] + [firstDay])))
+    groceries[food]['lastBought'] = min(foods[food])
+    if groceries[food]['meanWaitTime'] < groceries[food]['lastBought']:
+        print(food)
+
+#print(json.dumps(groceries,indent=2))
+
